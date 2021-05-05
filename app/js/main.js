cccaa472c8b526ea3331918b07ef43fc45c732eb
liss5555 <liss5555@rambler.ru>
2021-05-05 16:30:42 +0300

    added login.html

--- app/js/main.js
+++ app/js/main.js
@@ -61,15 +61,70 @@ $(function(){
 	
 	new WOW().init();
 
-	// $(".js-range-slider").ionRangeSlider({
-	// 	type: "double",
-	// 	min: 0,
-	// 	max: 1000,
-	// 	from: 0,
-	// 	to: 600,
-	// 	prefix: "$"
+
+	// ==========
+	var $range = $(".js-range-slider"),
+  $inputFrom = $(".js-input-from"),
+  $inputTo = $(".js-input-to"),
+  instance,
+  min = 0,
+  max = 1000,
+  from = 0,
+  to = 0;
 	
-	// });
+
+$range.ionRangeSlider({
+  skin: "round",
+  type: "double",
+  min: min,
+  max: max,
+  from: 60,
+  to: 600,
+  onStart: updateInputs,
+  onChange: updateInputs
+	
+});
+instance = $range.data("ionRangeSlider");
+
+function updateInputs(data) {
+  from = data.from;
+  to = data.to;
+
+  $inputFrom.prop("value", from);
+  $inputTo.prop("value", to);
+}
+
+$inputFrom.on("input", function() {
+  var val = $(this).prop("value");
+
+  // validate
+  if (val < min) {
+    val = min;
+  } else if (val > to) {
+    val = to;
+  }
+
+  instance.update({
+    from: val
+  });
+});
+
+$inputTo.on("input", function() {
+  var val = $(this).prop("value");
+
+  // validate
+  if (val < from) {
+    val = from;
+  } else if (val > max) {
+    val = max;
+  }
+
+  instance.update({
+    to: val
+  });
+});
+
+	// ==========
 
 	// $('.icon-th-list').on('click', function(){
 	// 	$('.product__item.center').addClass('list');
