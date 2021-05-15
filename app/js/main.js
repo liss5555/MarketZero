018fb35cf8c35d90c2dfacdcc40111470b61ec6d
liss5555 <liss5555@rambler.ru>
2021-05-15 16:30:39 +0300

    added singl-product.html

--- app/js/main.js
+++ app/js/main.js
@@ -23,7 +23,7 @@ $(function(){
 		normalFill: "#fff",
 		ratedFill: "#ffc000",
 		fullStar: true,
-		spacing: "5px",
+		spacing: "5px"
 		// starSvg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>',
 		// starSvg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>',
 
@@ -36,6 +36,15 @@ $(function(){
 		ratedFill: "#ffc000",
 		fullStar: true,
 	});
+
+	// $(".list__item-star.rate-star20.rate-star").rateYo({
+	// 	rating: 5,
+	// 	starWidth: "30px",
+	// 	normalFill: "#fff",
+	// 	ratedFill: "#000",
+	// 	fullStar: true,
+	// 	spacing: "3px"
+	// });
 
 
 
